68e502be3f19a30943ff16f4aa3913209f99823a
neuroleptique <corn.xiao@gmail.com>
2022-10-19 23:30:08 -0400

    add barista login function

--- routes/main.js
+++ routes/main.js
@@ -7,13 +7,13 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
-// router.get("/profile", ensureAuth, postsController.getProfile);
-// router.get("/feed", ensureAuth, postsController.getFeed);
-router.get("/login", authController.getLogin);
-router.post("/login", authController.postLogin);
-router.get("/logout", authController.logout);
-router.get("/ownersignup", authController.getSignup);
-router.post("/ownersignup", authController.postSignup);
+router.get("/baristaprofile", ensureAuth, postsController.getProfile);
+// router.get("/baristafeed", ensureAuth, postsController.getFeed);
+router.get("/baristalogin", authController.getLogin);
+router.post("/baristalogin", authController.postLogin);
+router.get("/baristalogout", authController.logout);
+// router.get("/ownersignup", authController.getSignup);
+// router.post("/ownersignup", authController.postSignup);
 router.get("/baristasignup", authController.getSignup);
 router.post("/baristasignup", authController.postSignup);
 
