23dd5bba577ef872b1d2a7d106c35ee67e2e5454
neuroleptique <corn.xiao@gmail.com>
2023-02-09 23:23:12 -0500

    add frontend logic for cloudinary upload

--- public/js/main.js
+++ public/js/main.js
@@ -200,3 +200,47 @@ async function updateAddress() {
   }
   
 }
+
+async function fetchSignature() {
+  const signResponse = await fetch('/barista/signuploadform', {
+    method: "get"
+  });
+  const signData = await signResponse.json();
+
+  const url = "https://api.cloudinary.com/v1_1/" + signData.cloudname + "/auto/upload";
+  const form = document.getElementById("uploadPhoto");
+  console.log(signData)
+
+  form.addEventListener("submit", (e) => {
+      e.preventDefault();
+  
+      const files = document.querySelector("[type=file]").files;
+      const formData = new FormData();
+  
+      // Append parameters to the form data. The parameters that are signed using 
+      // the signing function (signuploadform) need to match these.
+      for (let i = 0; i < files.length; i++) {
+          let file = files[i];
+          formData.append("file", file);
+          formData.append("api_key", signData.apikey);
+          formData.append("timestamp", signData.timestamp);
+          formData.append("signature", signData.signature);
+          formData.append("eager", "c_thumb,h_150,w_150, g_face:center");
+          formData.append("folder", "profile_photos");
+  
+          fetch(url, {
+              method: "POST",
+              body: formData
+          })
+          .then((response) => {
+              return response.text();
+          })
+          .then((data) => {
+              console.log(JSON.parse(data))
+              let str = JSON.stringify(JSON.parse(data), null, 4);
+              document.getElementById("formdata").innerHTML += str;
+          });
+      }
+  });
+
+}
